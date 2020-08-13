f31c55189523baf18c808ccebc16f192785be1e4
Rob Gagne <rob@robgagne.com>
2020-08-13 10:11:37 +0900

    switch duck to mix

--- src/ios/NativeAudio.m
+++ src/ios/NativeAudio.m
@@ -35,7 +35,7 @@ NSString* INFO_VOLUME_CHANGED = @"(NATIVE AUDIO) Volume changed.";
     
     // Allows the application to mix its audio with audio from other apps.
     if (![session setCategory:AVAudioSessionCategoryPlayback
-                  withOptions:AVAudioSessionCategoryOptionDuckOthers
+                  withOptions:AVAudioSessionCategoryOptionMixWithOthers
                         error:&setCategoryError]) {
 
         NSLog (@"Error setting audio session category.");
